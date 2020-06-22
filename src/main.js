c20f0425e5289669472a0902b708b4b36cc4b851
alex_klymenko <klymenko.sasha21@gmail.com>
2020-06-22 13:26:20 +0300

    2. generate menu template

--- src/main.js
+++ src/main.js
@@ -13,3 +13,12 @@ const generateTripInfoTemplate = () => {
     </section>`
   );
 };
+
+const generateTripMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
